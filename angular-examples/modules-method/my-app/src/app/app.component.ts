2a6ee8433ee850e3c026a4a0c3c47401d840d281
Shuheb Ahmed <shuhebahmed@Shuhebs-MacBook-Pro.local>
2020-04-22 15:16:47 +0100

    added angular modules method

---
+++ angular-examples/modules-method/my-app/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {AgGridAngular} from '@ag-grid-community/angular';
+import {AllModules} from '@ag-grid-enterprise/all-modules';
+
+// add license key as follows
+import {LicenseManager} from '@ag-grid-enterprise/core';
+LicenseManager.setLicenseKey('Enter your license key');
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+
+  title = 'app';
+
+  columnDefs = [
+    {headerName: 'Model', field: 'model'},
+    {headerName: 'Make', field: 'make', },
+    {headerName: 'Price', field: 'price'}
+  ];
+
+
+  rowData: any;
+
+  modules = AllModules;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.rowData = this.http.get('https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/rowData.json');
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data );
+    const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
+}
